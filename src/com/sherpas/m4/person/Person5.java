f5c79d4f064733b702fbb6bdb5ee920ead84d93c
Richard Li <lichard49@gmail.com>
2013-02-14 06:39:55 -0500

    Initial Structure and Person 5
    Establishing the initial folder structure and the Person5.java file.

---
+++ src/com/sherpas/m4/person/Person5.java
@@ -0,0 +1,46 @@
+package com.sherpas.m4;
+
+public class Person5 
+{
+	/** Holds the persons real name */
+	private String name;
+	
+  	/**
+	 * The constructor, takes in the persons
+	 * name
+	 * @param pname the person's real name
+	 */
+	public Person5(String pname) 
+	{
+		name = pname;
+	}
+	
+  	/**
+	 * This method should take the string
+	 * input and return its characters rotated
+	 * 3 positions.
+	 * given "gtg123b" it should return
+	 * "123bgtg".
+	 *
+	 * @param input the string to be modified
+	 * @return the modified string
+	 */
+	private String calc(String input) 
+	{
+		
+		return null;
+	}
+	
+	/**
+	 * Return a string rep of this object
+	 * that varies with an input string
+	 *
+	 * @param input the varying string
+	 * @return the string representing the 
+	 *         object
+	 */
+	public String toString(String input) 
+	{
+	  return name + calc(input);
+	}
+}
